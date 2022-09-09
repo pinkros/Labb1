ea09c52d607eea9977f6c3abdcde0122244c5f50
pinkros <arvid.leimar@iths.se>
2022-09-09 11:58:09 +0200

    Labb1 - vi kommer ngn vart?

--- Labb1/Labb1/Program.cs
+++ Labb1/Labb1/Program.cs
@@ -27,3 +27,60 @@ Total = 5836428677242*/
 //För varje hittad delsträng:
 //      Skriv ut hela strängen med delsträngen markerad i annan färg.
 //skriv ut summan av värdet av alla hittade delsträngar.
+
+//Fråga användaren om en sträng och spara den.
+Console.WriteLine("Hej! skriv in en sträng:");
+string input = Console.ReadLine();
+var sum = 0L;
+var indexOfFirstNumOfSubStr = 0;
+var indexOfLastNumOfSubStr = 0;
+var lengthOfSubstring = 0;
+
+
+
+//Hitta delsträngar som börjar och slutar på samma tal och är minst 3 tecken långa.
+for (int i = 0; i < input.Length; i++)
+{
+	if (int.TryParse(input[i]+"", out int currentNum))
+	{
+		
+		if (indexOfFirstNumOfSubStr == 0) { indexOfFirstNumOfSubStr = i; }
+
+		for (int j = i+1; j < input.Length; j++)
+		{
+			if (int.TryParse(input[j]+"", out int dummynum) == false)
+			{
+				lengthOfSubstring = 0;
+				indexOfFirstNumOfSubStr = 0;
+				break; 
+			}
+            else if ( int.Parse(input[j]+"") == currentNum)
+            {
+				indexOfLastNumOfSubStr = j;
+				lengthOfSubstring = j - indexOfFirstNumOfSubStr + 1;
+				break;
+            }
+
+
+        }
+
+		if (lengthOfSubstring >= 3 )
+		{
+			sum += long.Parse(input.Substring(indexOfFirstNumOfSubStr, lengthOfSubstring));
+			Console.Write(input.Substring(0, indexOfFirstNumOfSubStr));
+
+			Console.ForegroundColor = ConsoleColor.Red;
+
+			Console.Write(input.Substring(indexOfFirstNumOfSubStr,lengthOfSubstring));
+
+			Console.ResetColor();
+
+			Console.Write(input.Substring(indexOfLastNumOfSubStr + 1) + "\n");
+
+			indexOfFirstNumOfSubStr = 0;
+
+        }
+	}
+}
+
+Console.WriteLine($"Det totala värdet av alla delsträngar är: {sum}");
